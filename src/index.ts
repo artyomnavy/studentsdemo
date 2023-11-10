31a6b7ed8e6733a2edfaf739763f3199d276ec6a
Artyom Parygin <arteme.parygin@yandex.ru>
2023-11-10 22:11:09 +0300

    fix message

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
-  let helloMessage = 'Hello Samurais!!!';
+  let helloMessage = 'Hello Samurai!';
   res.send(helloMessage);
 });
 
